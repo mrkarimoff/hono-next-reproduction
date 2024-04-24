7e4cc9ffc8705d25e03817753274abe3a951208d
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-04-24 19:18:28 +0500

    ingerate db with turso

--- apps/hono/drizzle.config.ts
+++ apps/hono/drizzle.config.ts
@@ -1,11 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import type { Config } from "drizzle-kit";
 
 export default {
   schema: "./db/schema.ts",
   out: "./db/migrations",
-  driver: "better-sqlite",
+  driver: "turso",
   dbCredentials: {
-    url: "./sqlite.db",
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN,
   },
   verbose: true,
 } satisfies Config;
