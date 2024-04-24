387ec69c736db60d76d4c12b28c60e279c33ae29
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-04-24 21:26:36 +0500

    wip:set-up auth with clerk

--- apps/hono/src/index.ts
+++ apps/hono/src/index.ts
@@ -2,8 +2,25 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import usersRoute from "./routes/users";
 import todosRoute from "./routes/todos";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
 const app = new Hono();
+
+app.use("*", clerkMiddleware());
+app.get("/", (c) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    return c.json({
+      message: "You are not logged in.",
+    });
+  }
+
+  return c.json({
+    message: "You are logged in!",
+    userId: auth.userId,
+  });
+});
 
 const routes = app.route("/users", usersRoute).route("/todos", todosRoute);
 
