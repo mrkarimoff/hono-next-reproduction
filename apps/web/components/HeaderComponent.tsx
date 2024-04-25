2d363f174b365d2f3fc512df3a96cfa96fdae237
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-04-25 19:52:26 +0500

    implement team switcher with shadcn comboBox component

--- apps/web/components/HeaderComponent.tsx
+++ apps/web/components/HeaderComponent.tsx
@@ -4,6 +4,7 @@ import { ModeToggle } from "./ModeToggle";
 import Container from "./Container";
 import { SignInButton, SignOutButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
+import TeamSwitcher from "./TeamSwitcher";
 
 const HeaderComponent = () => {
   const { userId } = auth();
@@ -11,9 +12,13 @@ const HeaderComponent = () => {
   return (
     <div className="w-full h-20 border-b-2">
       <Container className="flex justify-between h-full items-center">
-        <Link className="text-xl" href={"/"}>
-          Next-Hono MVP
-        </Link>
+        <div className="flex gap-3 items-center">
+          <Link className="text-lg" href={"/"}>
+            Next-Hono MVP
+          </Link>
+          <TeamSwitcher />
+        </div>
+
         <div className="flex items-center gap-3">
           <ModeToggle />
           {userId ? (
