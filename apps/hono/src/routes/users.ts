5fc335c6579b63e8684d35b2008321a0dab15ad0
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-04-26 21:08:02 +0500

    wip: create endpoint for getting user by id

--- apps/hono/src/routes/users.ts
+++ apps/hono/src/routes/users.ts
@@ -5,32 +5,37 @@ import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 
 const app = new Hono()
-  .get("/", async (c) => {
-    const auth = getAuth(c);
-    const clerkClient = c.get("clerk");
+  .get(
+    "/:id",
+    zValidator(
+      "query",
+      z.object({
+        id: z.string(),
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const clerkClient = c.get("clerk");
+      const id = c.req.valid("query").id;
 
-    if (!auth?.userId) {
-      return c.json(
-        {
-          data: null,
-          userId: null,
-          message: "You are not logged in.",
-        },
-        401
-      );
+      if (!auth?.userId) {
+        return c.json({ data: null, error: "You are not logged in." }, 401);
+      }
+
+      const result = await db.query.users.findFirst({
+        where: { id: id },
+      });
+      const user = await clerkClient.users.getUser(auth.userId);
+
+      console.log("Authenticated user: ", user);
+
+      return c.json({
+        data: result,
+        userId: auth.userId,
+        message: "You are logged in!",
+      });
     }
-
-    const result = await db.query.users.findMany();
-    const user = await clerkClient.users.getUser(auth.userId);
-
-    console.log("Authenticated user: ", user);
-
-    return c.json({
-      data: result,
-      userId: auth.userId,
-      message: "You are logged in!",
-    });
-  })
+  )
   .post(
     "/",
     zValidator(
